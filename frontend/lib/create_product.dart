cb450f4d888f6f550e13d0e5c475178f9ce32f2f
aidaiym <omurbekovnaaidaiym@gmail.com>
2023-03-01 22:00:26 +0600

    remove settings.py

--- frontend/lib/create_product.dart
+++ frontend/lib/create_product.dart
@@ -97,7 +97,7 @@ class _CreateProductPageState extends State<CreateProductPage> {
                   }
                 });
               },
-              child: Text('Create Product'),
+              child: const Text('Create Product'),
             )
           ],
         ),
